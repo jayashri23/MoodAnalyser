57305424aa7a61ad59eeb0a8080a2a30c7af3d70
jayashri23 <jsjankar23@gmail.com>
2023-01-03 22:45:59 +0530

    Test case Mood Analyser

--- src/com/moodanalyser/MoodAnalyser.java
+++ src/com/moodanalyser/MoodAnalyser.java
@@ -1,6 +1,14 @@
 package com.moodanalyser;
 
 public class MoodAnalyser {
+    private String message;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
     public String analyseMood(String message) {
         if (message.contains("Sad"))
             return "SAD";
--- src/com/moodanalyser/MoodAnalyserTest.java
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -6,16 +6,18 @@ import static org.junit.Assert.assertEquals;
 
 public class MoodAnalyserTest {
     @Test
-    public void given_SadMood_Should_Return_Sad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am In a Sad Mood");
-        assertEquals("SAD", mood);
+        public void given_SadMood_Should_Return_Sad () {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            String mood = moodAnalyser.analyseMood("I am In a Sad Mood");
+            assertEquals("SAD", mood);
 
+        }
+
+        @Test
+        public void given_AnyMood_Should_Return_HAPPY () {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            String mood = moodAnalyser.analyseMood("I am in any Mood");
+            assertEquals("HAPPY", mood);
+        }
     }
-    @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("I am in any Mood");
-        assertEquals("HAPPY", mood);
-    }
-}
+
