07a9d47748655620bdb85401c6ffc9c6b633bfe6
jayashri23 <jsjankar23@gmail.com>
2023-01-03 22:41:25 +0530

    Test case Pass method

---
+++ src/com/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,21 @@
+package com.moodanalyser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MoodAnalyserTest {
+    @Test
+    public void given_SadMood_Should_Return_Sad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am In a Sad Mood");
+        assertEquals("SAD", mood);
+
+    }
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in any Mood");
+        assertEquals("HAPPY", mood);
+    }
+}
